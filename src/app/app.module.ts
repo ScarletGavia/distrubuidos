e3c18ba22c071794387b2002d849ff05d75b38aa
ScarletGavia <scarlet.gavia@gmail.com>
2023-11-28 20:04:07 -0300

    correccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { FootComponent } from './foot/foot.component';
+import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
+import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FootComponent,
+    IniciosesionComponent,
+    CrearCuentaComponent
   ],
   imports: [
     BrowserModule
